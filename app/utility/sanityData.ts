10ebbfe491d759dc8c1f81f4fdd2b85800bb2e4f
Martin K <martinkz@users.noreply.github.com>
2023-06-07 19:35:01 +0100

    Initial commit

---
+++ app/utility/sanityData.ts
@@ -0,0 +1,59 @@
+const DATASET = "production";
+const QUERY = encodeURIComponent('*[!(_id in path("drafts.**"))]');
+const PROJECT_URL = `https://${process.env.SANITY_PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`;
+
+export async function getSanityData(url: string) {
+	return await fetch(url)
+		.then((res) => res.json())
+		.then(({ result }) => {
+			console.log(result);
+			return result;
+		})
+		.catch((err) => console.error(err));
+}
+
+// Find the _ref properties (which come from a reference field or image) within the Sanity json data
+// and expand the object with the actual data from the referenced object elsewhere in the data
+function expandRefs(obj, originalData = null) {
+	if (originalData === null) {
+		originalData = obj;
+	}
+	if (Array.isArray(obj)) {
+		for (let elem of obj) {
+			expandRefs(elem, originalData);
+		}
+	} else if (typeof obj === "object") {
+		for (let key in obj) {
+			if (key === "_ref") {
+				obj["__expandedRef"] = findById(obj._ref, originalData);
+			}
+			expandRefs(obj[key], originalData);
+		}
+	}
+}
+
+function findById(id, data) {
+	return data.filter((item) => item._id === id);
+}
+
+function splitByType(data) {
+	const result = {};
+	for (const item of data) {
+		const { _type } = item;
+		result[_type] = [...(result[_type] || []), item];
+	}
+	return result;
+}
+
+function sortBoxItems(data) {
+	return data.box.sort((a, b) => a.orderRank.localeCompare(b.orderRank));
+}
+
+export async function getData() {
+	let sanityData = await getSanityData(PROJECT_URL);
+	expandRefs(sanityData);
+	sanityData = splitByType(sanityData);
+	sortBoxItems(sanityData);
+	// console.log(sanityData.homepage[0].boxes)
+	return sanityData;
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// const path = require("path");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	sassOptions: {
+		// includePaths: [path.join(__dirname, "styles")],
+		sourceMap: true,
+	},
+	// productionBrowserSourceMaps: true,
+};
+
+module.exports = nextConfig;
