c062c29da54f3b13a23e4529beb984e8f6725d8b
Martin K <martinkz@users.noreply.github.com>
2023-06-12 00:03:49 +0100

    Update donation page

--- app/[page]/page.tsx
+++ app/[page]/page.tsx
@@ -20,7 +20,10 @@ export async function generateMetadata({ params }: { params: { page: string } })
 export default async function Page({ params }: { params: { page: string } }) {
 	const { page } = await getData();
 	const the_page = page.find((page: any) => page.slug.current === params.page);
+	// console.log(the_page._id);
+
 	const isHaralanPage = the_page._id === "24650ce2-d6cf-474d-8123-70deb22d1dd6";
+	const isDonatePage = the_page._id === "5fab18a4-5451-4a2b-835a-6f1036fade95";
 
 	return (
 		<>
@@ -32,6 +35,13 @@ export default async function Page({ params }: { params: { page: string } }) {
 			<div className="page-wrap">
 				{isHaralanPage && <SimpleCarousel images={["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"]} />}
 				<div dangerouslySetInnerHTML={portableTextToHtml(the_page.body)}></div>
+				{isDonatePage && (
+					<div className="donate-wrap">
+						<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=XYTKARDXUW7U4" target="_blank" className="btn">
+							Donate now
+						</a>
+					</div>
+				)}
 			</div>
 		</>
 	);
